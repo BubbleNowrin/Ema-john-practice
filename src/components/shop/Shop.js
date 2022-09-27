a6c2986a970e5011fd385559f98be866c90c16bd
NowrinIslamMim <nowrin02@gmail.com>
2022-09-27 19:19:14 +0600

    new commit

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -13,9 +14,38 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, [])
 
+    useEffect(() => {
+        const storedCart = getStoredCart();
+        // console.log(storedCart);
+        const savedCart = [];
+        for (const id in storedCart) {
+            const addedProducts = products.find(product => product.id === id);
+            // console.log(addedProducts);
+            if (addedProducts) {
+                const quantity = storedCart[id];
+                addedProducts.quantity = quantity;
+                savedCart.push(addedProducts);
+            }
+            // console.log(addedProducts);
+            setCart(savedCart);
+        }
+    }, [products])
+
     const addToCart = (product) => {
-        const newCart = [...cart, product];
+
+        let newCart = [];
+
+        const exists = cart.find(p => p.id === product.id);
+        if (!exists) {
+            product.quantity = 1;
+            newCart = [...cart, product];
+        } else {
+            const rest = cart.filter(p => p.id !== product.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists];
+        }
         setCart(newCart);
+        addToDb(product.id);
     }
     return (
         <div className='shop-container'>
